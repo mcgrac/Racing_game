1b1afc144ae107e91b9af8c9711986867c6037bb
mcgrac <112167740+mcgrac@users.noreply.github.com>
2025-12-03 14:20:58 +0100

    added textures from main

--- Source/Level1.cpp
+++ Source/Level1.cpp
@@ -1,2 +1,22 @@
 #include "Level1.h"
 
+Level1::Level1(ModulePhysics* physics, Module* _listener)
+{
+}
+
+Level1::~Level1()
+{
+}
+
+void Level1::Start()
+{
+	Texture2D floor = LoadTexture()
+}
+
+void Level1::Update()
+{
+}
+
+void Level1::CleanUp()
+{
+}
--- Source/Level1.h
+++ Source/Level1.h
@@ -1,12 +1,22 @@
 #pragma once
 #include "Map.h"
 
-class level1 : public Map {
+class Level1 : public Map {
 
 public:
+
+    Level1(ModulePhysics* physics, Module* _listener);
+    ~Level1();
+
+    void Start() override;
+    void Update() override;
+    void CleanUp() override;
 
 protected:
 
 private:
+
+    Texture2D floor;
+    Texture2D overMap;
 
 };
