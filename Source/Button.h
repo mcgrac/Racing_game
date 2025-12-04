e7e1c1758c2a4b34795fc4270b0d94440bd3b5dc
Orlock6400 <bernat.pazos@estudiantat.upc.edu>
2025-12-04 13:37:52 +0100

    Button class defined

--- Source/Button.h
+++ Source/Button.h
@@ -1,21 +1,41 @@
 #pragma once
 #include "raylib.h"
-#include <vector>
 
 class Button {
 
 public:
+    Button(Texture2D buttonUp,
+        Texture2D buttonDown,
+        Texture2D buttonSelected,
+        Sound pressSound, Sound selectSound,
+        Vector2 position,
+        float pressDuration = 0.2f);
 
-	Button(){}
+    ~Button() {}
 
-	~Button() {}
+    void Update();
+    void Draw();
+
+    void Select();
+    void Press();              // Call this from Scene
+    void Release();
+    bool IsPressed() const { return isPressed; }
+    bool IsSelected() const { return isSelected; }
+
+
 
 private:
+    Texture2D buttonUpSprite;
+    Texture2D buttonDownSprite;
+    Texture2D buttonSelectedSprite;
 
-public:
+    Sound pressSound;
+    Sound selectSound;
 
-private:
+    Vector2 position;
 
-	Texture2D
-
+    bool isPressed = false;
+    bool isSelected = false;
+    float pressTime = 0.0f;
+    float pressDuration;       // How long the button stays down
 };
