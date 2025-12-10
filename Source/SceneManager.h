a61aa760a9bd3cdd7010411d19f450b50df6c7a0
mcgrac <marcsie2503@gmail.com>
2025-12-10 22:49:57 +0100

    Update SceneManager.h

--- Source/SceneManager.h
+++ Source/SceneManager.h
@@ -1,5 +1,28 @@
 #pragma once
 
+#include "raylib.h"
+#include <vector>
+
+enum class GameSceneState {
+	MAIN_MENU,
+	PLAYING,
+	FINISH_RACE,
+	CHARACTER_SELECTION
+};
 class SceneManager {
+
+private:
+
+	int currentScreenID();
+
+public:
+
+	void Start();
+	void Update();
+
+	void SwitchScreen(int screenID);
+	void MoveSelector();
+
+
 
 };
