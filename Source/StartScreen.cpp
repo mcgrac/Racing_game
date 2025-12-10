f1c8c4731cf2dfc1447110530f2c1b9c1ccf194e
Xmacias365 <159776765+xavimacias365@users.noreply.github.com>
2025-12-10 14:59:35 +0100

    title screen I

---
+++ Source/StartScreen.cpp
@@ -0,0 +1,2 @@
+#include "StartScreen.h"
+
--- Source/StartScreen.h
+++ Source/StartScreen.h
@@ -1,1 +1,38 @@
 #pragma once
+#include "raylib.h"
+#include "button.h"
+#include "Scene.h"
+
+class StartScreen {
+private:
+	enum MainMenuOption {
+		START,
+		HELP,
+		BADGE,
+		QUIT,
+		MAIN_MENU_OPTION_COUNT
+	};
+
+	MainMenuOption selectedOption = START;
+	bool hasPressStart = false;
+	bool hasPressHelp = false;
+	bool hasPressBadge = false;
+	bool hasPressQuit = false;
+
+public:
+	StartScreen() {}
+	//void Update();
+	//void Draw();
+
+	bool GetHasPressStart() const { return hasPressStart; }
+	bool GetHasPressHelp() const { return hasPressHelp; }
+	bool GetHasPressBadge() const { return hasPressBadge; }
+	bool GetHasPressQuit() const { return hasPressQuit; }
+	
+	void Reset() {
+		hasPressStart = false;
+		hasPressHelp = false;
+		hasPressBadge = false;
+		hasPressQuit = false;
+	}
+};
